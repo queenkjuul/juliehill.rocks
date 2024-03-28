69ffdf7afef04e5cd4c36e6da59e105771f9aab8
queenkjuul <queenkjuul69@gmail.com>
2024-03-27 21:15:05 -0500

    240327: basic styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
-  import { Alert } from 'flowbite-svelte'
+  import { Alert, Button } from 'flowbite-svelte'
 </script>
 
 <div class="p-8">
-  <Alert>
+  <h1>Julie Hill</h1>
+  <Alert class="m-4">
     <span class="font-medium">Info alert!</span>
     Change a few things up and try submitting again.
   </Alert>
+  <Button class="m-2">BUTTON</Button>
+  <a>queenkjuul.com</a>
 </div>
