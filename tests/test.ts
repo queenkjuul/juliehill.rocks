4db8a458e952c0175693d1cfa141aab6997381c5
queenkjuul <queenkjuul69@gmail.com>
2024-03-29 22:20:36 -0500

    240329: Fix broken test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test'
 
 test('index page has expected h1', async ({ page }) => {
-	await page.goto('/')
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible()
+  await page.goto('/')
+  await expect(page.getByRole('heading', { name: 'Julie Hill' })).toBeVisible()
+  await expect(page.getByText('Frontend Engineer')).toBeVisible()
 })
