4d1ae4c6404f684c9c922fb6034c40d092ba5d09
queenkjuul <queenkjuul69@gmail.com>
2024-04-02 15:03:02 -0500

    240402: Github API: null-safety and no-auth bug

--- src/routes/code/+page.svelte
+++ src/routes/code/+page.svelte
@@ -11,13 +11,16 @@
   const component = RepoInfoCard
   const gap = 'gap-4'
 
-  $: pinnedNames = pinned?.map(({ name }) => name)
+  $: pinnedNames = pinned.map(({ name }) => name)
   $: ghRepoCardData = repos?.filter((repo: RepoCardData) => !pinnedNames.includes(repo.name))
 </script>
 
-<Heading class="text-center">Featured Repos</Heading>
-<Hr hrClass="mt-0 h-1 bg-primary-400 border-primary-400 mb-4 mx-auto w-1/2" />
-<MasonryLayout items={pinned} {component} {breakpoints} {gap} divClass="mb-4" />
+<!-- if no github token, we will only get public repos, not pinned rpos -->
+{#if pinned.length > 0}
+  <Heading class="text-center">Featured Repos</Heading>
+  <Hr hrClass="mt-0 h-1 bg-primary-400 border-primary-400 mb-4 mx-auto w-1/2" />
+  <MasonryLayout items={pinned} {component} {breakpoints} {gap} divClass="mb-4" />
+{/if}
 
 <Heading class="text-center">Public Repositories</Heading>
 <Hr hrClass="mt-0 h-1 bg-primary-400 border-primary-400 mb-4 mx-auto w-1/2" />
